4004855864c266b3409d7489768c87cfb343d5ce
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-07-01 21:39:25 +0530

    Http Requests

---
+++ Readme.md
@@ -0,0 +1,1 @@
+- Order of Routes is very important
--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Kids");
+app.get("/user", (req, res) => {
+  res.send("GET call");
 });
-
-app.use("/test", (req, res) => {
-  res.send("Welcome Users");
+app.post("/user", (req, res) => {
+  res.send("POST call");
+});
+app.patch("/user", (req, res) => {
+  res.send("PATCH call");
+});
+app.delete("/user", (req, res) => {
+  res.send("DELETE call");
 });
 app.listen(7777, () => {
   console.log("Server Listening on Port 7777");
