51eef95a79e1d94c7d283baffaf1a6a297344d5b
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-07-02 13:00:41 +0530

    Error Handler

--- src/app.js
+++ src/app.js
@@ -15,6 +15,21 @@ app.get("/user", userAuth, (req, res, next) => {
   res.send("Welcome To NodeJs World");
 });
 
+// Error Handler
+app.get("/userData", userAuth, (req, res, next) => {
+  throw new Error("Error!!!!!!");
+  res.send("Welcome To NodeJs World");
+  // try {
+  //   throw new Error("Error!!!!!!");
+  //   res.send("Welcome To NodeJs World");
+  // } catch (error) {
+  //   res.status(500).send("Something went wrong");
+  // }
+});
+app.use("/", (err, req, res, next) => {
+  if (err) res.status(500).send("Internal Server Error");
+});
+
 app.listen(7777, () => {
   console.log("Server Listening on Port 7777");
 });
