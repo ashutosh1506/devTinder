ac304fdb2cda68769d4fd336bddf2a035067f078
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-07-09 19:08:53 +0530

    Setup routing

--- frontend/README.md
+++ frontend/README.md
@@ -1,5 +1,25 @@
-# DevTinder UI
+## DevTinder UI
+
+# Steps
 
 - Created vite@latest
 - Setup Tailwind
 - Setup DaisyUI
+- created Navbar component
+- Setup Routing in App.jsx
+- Created Body Component
+  - Setup child components in this
+
+# Components
+
+- Navbar
+- Login
+- Profile
+- Body
+- Footer
+
+# Libraries installed
+
+1. Tailwind CSS
+2. DaisyUI
+3. react-router-dom
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
