5808c6df6510de42e6aaf9a6ed547feccece42ee
Ashutosh Gupta <guptaashutosh502@gmail.com>
2025-07-10 20:48:23 +0530

    card width changed

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -4,7 +4,7 @@ const UserCard = ({ user }) => {
   return (
     <div className="card bg-base-300 w-96 shadow-lg ">
       <figure className="px-10 pt-10">
-        <img src={photoURL} alt="Shoes" className="rounded-xl" />
+        <img src={photoURL} alt="Shoes" className="rounded-xl w-full" />
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
